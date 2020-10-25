2255d152ab06e7f003622de9bb3d64978c3bba2a
Yossi Eisenbach <yossie@ravtech.co.il>
2020-10-25 21:37:42 +0200

    add job_name script

--- job_name.py
+++ job_name.py
@@ -1,22 +1,15 @@
 import os
 import sys
 
-
-def isMultipleof3(n): 
-	
-	while ( n > 0 ): 
-		n = n - 3
-
-	if ( n == 0 ): 
-		return 1
-
-	return 0
-	
-i = int(os.environ['BUILD_NUMBER'])
-if ( isMultipleof3(i) == 1 ): 
-	print ("Job Name is" + ' ' + os.environ['JOB_NAME']) 
-	
-else: 
-	print (i, "is not a multiple of 3") 
+build_num = int(os.environ['BUILD_NUMBER'])
+devsion = 3
+modulo = build_num%devsion
+if modulo > 0:
+	print ("is not a multiple of 3")
 	sys.exit(1)
+else:
+	print ("Job Name is" + ' ' + os.environ['JOB_NAME'])
+
+
+
 
